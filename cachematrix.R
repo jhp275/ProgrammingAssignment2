4c35433747dfcb96b9db4f091fdc4e6d4e1d7d1b
Jay Patadia <jhp275@gmail.com>
2016-11-22 15:34:52 -0500

    edited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix defines functions set, get to write/read matrix resp.,  setInvMatrix/getInvMatrix to write/read Inverted Matrix resp.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInvMatrix <- function(UserInvMatrix) invMatrix <<- UserInvMatrix
+    getInvMatrix <- function() invMatrix
+    list(set = set, get = get,
+         setInvMatrix = setInvMatrix,
+         getInvMatrix = getInvMatrix)
 }
 
-
-## Write a short comment describing this function
+## cachesolve checks if Ivnerted Matrix is already cached in the memory, if not inverts matrix using solve
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    invMatrix <- x$getInvMatrix()
+    if(!is.null(invMatrix)) {
+        message("getting cached Inverted Matrix")
+        return(invMatrix)
+    }
+    mMatrix <- x$get()
+    invMatrix <- solve(mMatrix, ...)
+    x$setInvMatrix(invMatrix)
+    invMatrix
 }
